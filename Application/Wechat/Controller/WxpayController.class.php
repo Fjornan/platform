d787f0aa96b37d29e370245ec6f80a91b5fa485a
fjornan <475773796@qq.com>
2017-09-29 15:10:40 +0800

    。。

--- Application/Wechat/Controller/WxpayController.class.php
+++ Application/Wechat/Controller/WxpayController.class.php
@@ -26,4 +26,19 @@ class WxpayController extends ComController {
         // var_dump($data);
         $this->display();
     }
+
+    function wx_notify(){
+        Vendor('Wxpay.WxPay#Api');
+        $raw_xml = file_get_contents("php://input");
+        $notify = new \WxPayNotifyCallBack();
+        $notify->Handle(false);
+        $res = $notify->GetValues();
+        if($res['return_code'] ==="SUCCESS" && $res['return_msg'] ==="OK"){
+            libxml_disable_entity_loader(true);
+            $ret = json_decode(json_encode(simplexml_load_string($raw_xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+            \Think\Log::write('微信APP支付成功订单号'.$ret['out_trade_no'], \Think\Log::DEBUG);
+            //在此处处理业务逻辑部分
+            echo "支付成功";
+        }
+    }
 }
