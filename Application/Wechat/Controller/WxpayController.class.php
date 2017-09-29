0b91de1fb00a772310627dbad0e4a2525bf4b327
fjornan <475773796@qq.com>
2017-09-29 16:38:54 +0800

    ..

--- Application/Wechat/Controller/WxpayController.class.php
+++ Application/Wechat/Controller/WxpayController.class.php
@@ -30,9 +30,9 @@ class WxpayController extends ComController {
     }
     public function pay(){
         // 导入微信支付sdk
-        Vendor('Weixinpay.Weixinpay');
-        // import('Vendor.Weixinpay.Weixinpay');
-        $wxpay=new \Weixinpay();
+        // Vendor('Weixinpay.Weixinpay');
+        import('Vendor.Weixinpay.Weixinpay');
+        $wxpay=new Weixinpay();
         // 获取jssdk需要用到的数据
         $data=$wxpay->getParameters();
         // 将数据分配到前台页面
--- ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
+++ ThinkPHP/Library/Vendor/Weixinpay/Weixinpay.php
@@ -1,5 +1,5 @@
 <?php
-namespace Vendor\Weixinpay;
+namespace Wechat\Controller;
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
