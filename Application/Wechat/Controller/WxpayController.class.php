77f42bf522eac705857d1a6c606304ef23279adb
fjornan <475773796@qq.com>
2017-09-29 16:17:32 +0800

    ..

--- Application/Wechat/Controller/WxpayController.class.php
+++ Application/Wechat/Controller/WxpayController.class.php
@@ -47,7 +47,7 @@ class WxpayController extends ComController {
         // $url=U('Wechat/wxpay/pay',array('out_trade_no'=>$out_trade_no));
         // 前往支付
         // redirect($url);
-        $this->redirect('Wechat/Wxpay/pay',array('out_trade_no'=>$out_trade_no));
+        $this->redirect('wxpay/pay',array('out_trade_no'=>$out_trade_no));
     }
     
 }
