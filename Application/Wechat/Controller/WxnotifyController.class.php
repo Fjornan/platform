7b4cd110e770870e647a0ba2659b3cc2b3b4a7ae
fjornan <475773796@qq.com>
2017-10-02 22:38:02 +0800

    。。

--- Application/Wechat/Controller/WxnotifyController.class.php
+++ Application/Wechat/Controller/WxnotifyController.class.php
@@ -10,18 +10,9 @@ class WxnotifyController extends Controller{
 		
 	}
 	public function notify(){
-	    // ↓↓↓下面的file_put_contents是用来简单查看异步发过来的数据 测试完可以删除；↓↓↓
-	    // 获取xml
-	    $xml=file_get_contents('php://input', 'r');
-	    //转成php数组 禁止引用外部xml实体
-	    libxml_disable_entity_loader(true);
-	    $data= json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA));
-	    file_put_contents('./notify.text', $data);
-	    // ↑↑↑上面的file_put_contents是用来简单查看异步发过来的数据 测试完可以删除；↑↑↑
-	    // 导入微信支付sdk
 	    Vendor('Weixinpay.Weixinpay');
-	    $wxpay=new \Weixinpay();
-	    $result=$wxpay->notify();
+    	$wxpay=new \Weixinpay();
+    	$result=$wxpay->notify();
 	    if ($result) {
 	        // 验证成功 修改数据库的订单状态等 $result['out_trade_no']为订单id
 	        $id = $result['out_trade_no'];
