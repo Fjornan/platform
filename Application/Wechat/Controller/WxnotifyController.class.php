be5c5451c52ec577b7c704cf98d3d99cedf34061
fjornan <475773796@qq.com>
2017-10-03 00:06:29 +0800

    chognfu

--- Application/Wechat/Controller/WxnotifyController.class.php
+++ Application/Wechat/Controller/WxnotifyController.class.php
@@ -17,7 +17,7 @@ class WxnotifyController extends Controller{
 	    $result=$wxpay->notify();
 	    if ($result) {
 	        // 验证成功 修改数据库的订单状态等 $result['out_trade_no']为订单id
-	        $id = substr($result['out_trade_no'],8);
+	        $id = substr($result['out_trade_no'],14);
 	    	$db = M('order');
 	        $db->where('id='.$id)->setField('status',1);
 
