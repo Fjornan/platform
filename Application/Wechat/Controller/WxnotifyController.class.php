379d6cd0ba8c7f6f9fb2529524056aabab0901f5
fjornan <475773796@qq.com>
2017-10-03 00:03:39 +0800

    重复订单

--- Application/Wechat/Controller/WxnotifyController.class.php
+++ Application/Wechat/Controller/WxnotifyController.class.php
@@ -17,7 +17,7 @@ class WxnotifyController extends Controller{
 	    $result=$wxpay->notify();
 	    if ($result) {
 	        // 验证成功 修改数据库的订单状态等 $result['out_trade_no']为订单id
-	        $id = $result['out_trade_no'];
+	        $id = substr($result['out_trade_no'],8);
 	    	$db = M('order');
 	        $db->where('id='.$id)->setField('status',1);
 
