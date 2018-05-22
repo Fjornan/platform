0e7adb58157728685eed628be72935fddcee0a2f
fjornan <475773796@qq.com>
2018-05-22 19:19:59 +0800

    替换

--- Application/Wechat/Conf/config.php
+++ Application/Wechat/Conf/config.php
@@ -39,7 +39,7 @@ return array(
     'WEIXINPAY_CONFIG'       => array(
     'APPID'              => 'wx205959c817c7f864', // 微信支付APPID
     'MCHID'              => '1488897962', // 微信支付MCHID 商户收款账号
-    'KEY'                => '6E34F2C4978F3E90E70CADCD427D1E96', // 微信支付KEY
+    'KEY'                => 'Jhangshiyi123456jinghangshiyipan', // 微信支付KEY
     'APPSECRET'          => 'da33bab21ed082590ad954080662092a', // 公众帐号secert (公众号支付专用)
     'NOTIFY_URL'         => 'http://wx.jinghangkuajing.com/index.php/Wechat/Wxnotify/notify', // 接收支付状态的连接
     ),
